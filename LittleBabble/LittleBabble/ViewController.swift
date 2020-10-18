4db5fd3fb3379fb1d0abc07862c8e801277f3d01
GhislaineNg <71164774+GhislaineNg@users.noreply.github.com>
2020-10-18 12:43:13 -0700

    Add files via upload

---
+++ LittleBabble/LittleBabble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleBabble
+//
+//  Created by Ghislaine Ng yee hui on 10/17/20.
+//  Copyright © 2020 Ghislaine Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
